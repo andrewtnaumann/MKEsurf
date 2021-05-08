fd1a69285d27f9a9b7fd2d89fd7f03605fef65bf
Andrew Naumann <naumaa7@gmail.com>
2021-05-08 12:43:08 -0500

    new

--- londonmapbot-tweet.R
+++ londonmapbot-tweet.R
@@ -1,34 +1,53 @@
+# remove and add t YAML file when done!!
+library(twitteR)
+library(rvest)
+library(stringr)
+library(dplyr)
+
 # Create Twitter token
 londonmapbot_token <- rtweet::create_token(
-  app = "londonmapbot",
+  app = "MKEsurf",
   consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
   access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
-# Generate random coordinates within specific limits
-lon <- round(runif(1, -0.489, 0.236), 4)
-lon <- format(lon, scientific = FALSE)
-lat <- round(runif(1, 51.28, 51.686), 4)
+# scrape data 
+current_url <- "http://magicseaweed.com/Milwaukee-Surf-Report/962/"
 
-# Build URL and fetch image from Mapbox API
-img_url <- paste0(
-  "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/",
-  paste0(lon, ",", lat),
-  ",15,0/600x400@2x?access_token=",
-  Sys.getenv("MAPBOX_PUBLIC_ACCESS_TOKEN")
-)
+current <- read_html(current_url) %>%
+  html_nodes(".msw-fc-current-v0 .row") %>%
+  html_text()  %>% str_split("        ")
 
-# Download the image to a temporary location
-temp_file <- tempfile()
-download.file(img_url, temp_file)
+body_nodes <- current %>% 
+  html_node("body") %>% 
+  html_children()
 
-# Build the status message (text and URL)
-latlon_details <- paste0(
-  lat, ", ", lon, "\n",
-  "https://www.openstreetmap.org/#map=17/", lat, "/", lon, "/"
-)
+body_nodes 
+body_nodes %>% html_children()
+
+current <- unlist(current)
+
+# clean up empty cells and split phrases for easy use
+cond <- current
+cond <- cond[!str_detect(cond, "Wind Swell")]
+cond <- cond[!str_detect(cond, "Secondary Swell")]
+cond[cond==""] <- NA
+cond <- cond[complete.cases(cond)]
+cond <- str_trim(cond, "both") %>%
+  str_split(" ")
+
+waves <- cond[[1]]
+wind <- cond[[2]]
+swell <- cond[[3]]
+weather <- cond[[4]]
+
+current_surf <- str_c(strftime(Sys.time(),"%I:%M %p"),":"," Waves ", waves, " with a ", swell[2], " of ", 
+                      swell[6], " at ", swell[8], ". ", wind[3], " wind at ", wind[1], ". ", weather[1], 
+                      "and ", weather[3], " Water temp: ", weather[5], weather[6], ". #elporto #surf")
+current_surf
+
 
 # Post the image to Twitter
 rtweet::post_tweet(
---
+++ noaa-tweet.R
@@ -0,0 +1,85 @@
+library(readr)
+library(ggplot2)
+library(lubridate)
+
+df_import <- readr::read_delim("https://www.ndbc.noaa.gov/data/realtime2/45013.txt", delim = " ")
+
+# define "good" conditions
+surf_wv_ht <- 1 # meter
+surf_wv_per <- 5 # seconds
+
+df_manipulate <- df_import %>%
+  # save only columns we want and rename some columns
+  select('#YY', ' MM', 'DD', 'hh', 'mm', 'WDIR', 'WSPD', 'GST', 'WVHT' = ' WVHT', 'DPD' = '  DPD',
+         ' WTMP', ' ATMP') %>%
+  # remove the first row with measurement units
+  slice(-1) %>%
+  mutate_if(is.character, as.numeric) %>%
+  mutate(date = as.POSIXct(paste(`#YY`, "-", ` MM`, "-", `DD`, " ", `hh`, ":", `mm`, sep = ''), format = "%Y-%m-%d %H:%M")) %>%
+  select(-c("#YY", " MM", "DD", "hh", "mm")) %>%
+  arrange(date) %>%
+  # take care of missing values - missing values can either be 0 or missing observation
+  replace(is.na(.), 0)
+
+# define "good" surf
+df_vars <- df_manipulate %>%
+  mutate(surfs_up = ifelse(WVHT >= surf_wv_ht & DPD >= surf_wv_per, 1, 0)
+  ) %>%
+  arrange(desc(date))
+
+rose_breaks <- c(0, 360/32, (1/32 + (1:15 / 16)) * 360, 360)
+
+rose_labs <- c(
+  "North", "North-Northeast", "Northeast", "East-Northeast",
+  "East", "East-Southeast", "Southeast", "South-Southeast",
+  "South", "South-Southwest", "Southwest", "West-Southwest",
+  "West", "West-Northwest", "Northwest", "North-Northwest",
+  "North"
+)
+
+wind_dir <- tibble(
+  wd = (0:16 / 16) * 360
+)
+df_wind_dir <- wind_dir %>%
+  mutate(
+    rose = cut(
+      wd,
+      breaks = rose_breaks,
+      labels = rose_labs,
+      right = FALSE,
+      include.lowest = TRUE
+    )
+  )
+
+df_recent <- df_vars %>%
+  slice(1) %>%
+  mutate(dummy=TRUE) %>%
+  left_join(df_wind_dir %>% mutate(dummy=TRUE)) %>%
+  filter(WDIR >= wd
+         ) %>%
+  select(-dummy) %>%
+  arrange(desc(wd)) %>%
+  slice(1)
+
+# is there is surf within the past hour, send a text
+if (df_recent$WVHT >= 1) {
+  
+  current_surf <- str_c(strftime(df_recent$date - hours(5)),":"," Waves "
+                        , round(df_recent$WVHT *3.28, 2), " feet at ", df_recent$DPD
+                        , " seconds. ", df_recent$rose, " wind at "
+                        , df_recent$WSPD*1.15, " mph. ", " Water temp: "
+                        , (df_recent$` WTMP` * (9/5))  + 32, " degrees. ", " Air temp: "
+                        , (df_recent$` ATMP` * (9/5))  + 32, " degrees. #milwaukee #surf")
+  current_surf
+  
+  # Post the image to Twitter
+  rtweet::post_tweet(
+    status = current_surf,
+    media = temp_file,
+    token = londonmapbot_token
+  )
+  
+} else
+{
+  stop()
+}
