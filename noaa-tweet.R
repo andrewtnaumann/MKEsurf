3efbf3fb17e94e69e224669ad74a89fce2d99ed5
Andrew Naumann <naumaa7@gmail.com>
2021-05-10 14:31:37 -0500

    clean up a bit for efficiency

--- noaa-tweet.R
+++ noaa-tweet.R
@@ -1,13 +1,7 @@
 rm(list=ls())
 
-library(readr)
-library(lubridate)
-library(dplyr)
-library(rtweet)
-library(stringr)
-
 # Create Twitter token
-londonmapbot_token <- rtweet::create_token(
+londonmapbot_token <- create_token(
   app = "mkesurf",
   consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
@@ -15,30 +9,23 @@ londonmapbot_token <- rtweet::create_token(
   access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
-df_import <- readr::read_delim("https://www.ndbc.noaa.gov/data/realtime2/45013.txt", delim = " ")
-
 # define "good" conditions
 surf_wv_ht <- 1 # meter
 surf_wv_per <- 5 # seconds
 
-df_manipulate <- df_import %>%
+df_vars <- read_delim("https://www.ndbc.noaa.gov/data/realtime2/45013.txt", delim = " ") %>%
   # save only columns we want and rename some columns
   select('#YY', ' MM', 'DD', 'hh', 'mm', 'WDIR', 'WSPD', 'GST', 'WVHT' = ' WVHT', 'DPD' = '  DPD',
          ' WTMP', ' ATMP') %>%
   # remove the first row with measurement units
   slice(-1) %>%
+  slice(1) %>%
   mutate_if(is.character, as.numeric) %>%
   mutate(date = as.POSIXct(paste(`#YY`, "-", ` MM`, "-", `DD`, " ", `hh`, ":", `mm`, sep = ''), format = "%Y-%m-%d %H:%M")) %>%
   select(-c("#YY", " MM", "DD", "hh", "mm")) %>%
-  arrange(date) %>%
+  arrange(desc(date)) %>%
   # take care of missing values - missing values can either be 0 or missing observation
-  replace(is.na(.), 0)
-
-# define "good" surf
-df_vars <- df_manipulate %>%
-  mutate(surfs_up = ifelse(WVHT >= surf_wv_ht & DPD >= surf_wv_per, 1, 0)
-  ) %>%
-  arrange(desc(date))
+  replace(is.na(.), 0) 
 
 rose_breaks <- c(0, 360/32, (1/32 + (1:15 / 16)) * 360, 360)
 
@@ -65,7 +52,6 @@ df_wind_dir <- wind_dir %>%
   )
 
 df_recent <- df_vars %>%
-  slice(1) %>%
   mutate(dummy=TRUE) %>%
   left_join(df_wind_dir %>% mutate(dummy=TRUE)) %>%
   filter(WDIR >= wd
@@ -83,15 +69,14 @@ if (df_recent$WVHT >= 1) {
                         , round(df_recent$WSPD*1.15, 0), " mph. ", " Water temp: "
                         , round((df_recent$` WTMP` * (9/5))  + 32, 0), " degrees. ", " Air temp: "
                         , round((df_recent$` ATMP` * (9/5))  + 32, 0), " degrees. #milwaukee #surf")
-  current_surf
   
   # Post the image to Twitter
-  rtweet::post_tweet(
+  post_tweet(
     status = current_surf
     # , media = temp_file
     , token = londonmapbot_token
   )
   
-} else
-{
+} else {
+  
 }
